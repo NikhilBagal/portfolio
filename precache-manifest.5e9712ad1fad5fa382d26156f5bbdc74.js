f5cba9033b1dcfd96ab379c1f3fc68f963152ff5
Nikhil Bagal <nbagal02@gmail.com>
2020-01-21 03:36:29 +0530

    Updates

---
+++ precache-manifest.5e9712ad1fad5fa382d26156f5bbdc74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1021edcf36b6e76bec23b54a41647545",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cdfc3f56d594a53e2278",
+    "url": "/portfolio/static/css/main.71909558.chunk.css"
+  },
+  {
+    "revision": "9509cca5cfd2ea5dbeea",
+    "url": "/portfolio/static/js/2.8d984d06.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.8d984d06.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdfc3f56d594a53e2278",
+    "url": "/portfolio/static/js/main.f99072f1.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "5b09483a0f24bf66d6ff1eb6eb5df7f5",
+    "url": "/portfolio/static/media/NikEdit.5b09483a.png"
+  },
+  {
+    "revision": "21ea29a6d7890323751bb5d2879736bd",
+    "url": "/portfolio/static/media/desktop-design.21ea29a6.jpg"
+  }
+]);
